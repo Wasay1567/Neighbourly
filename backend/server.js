057450929643051b28d975a9622fa5c65520ad19
Muhammad-Asher-Sajid-Sheikh <ashersajidpk2005@gmail.com>
2026-01-24 21:05:25 +0500

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js'
-import { env } from './config/env.js'
-import { runMigrations } from './database/runMigrations.js'
-
-await runMigrations()
-
-app.listen(env.PORT, () =>
-  console.log(`Server running on port ${env.PORT}`)
-)
--- backend/src/config/db.js
+++
@@ -1,8 +0,0 @@
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
-import { env } from './env.js'
-
-export const db = await open({
-  filename: env.DB_PATH,
-  driver: sqlite3.Database
-})
