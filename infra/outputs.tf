90a0b5f26943bce37a92536757604f06466b3d6b
Abdul Wasay <irfansoomro370@gmail.com>
2026-05-01 19:01:17 +0500

    Improve Terraform output handling in deployment workflow and remove unused outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,18 +2,3 @@ output "ec2_public_ip" {
   description = "The public IP address of the EC2 instance"
   value       = aws_instance.app_instance.public_ip
 }
-
-output "ec2_instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.app_instance.id
-}
-
-output "ec2_security_group_id" {
-  description = "The security group ID"
-  value       = aws_security_group.security_group.id
-}
-
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.my_bucket.id
-}
